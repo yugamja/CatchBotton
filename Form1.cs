52de571cf6e9d9da9f794316bc8840f7d3cb3300
yugamja <timun@naver.com>
2026-03-12 17:09:44 +0900

    [3단계] 점수판 추가와 성공 시 버튼 크기 축소 기능 추

--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,14 @@ namespace CatchBotton
         SoundPlayer runSound = new SoundPlayer(@"run.wav");
         SoundPlayer catchSound = new SoundPlayer(@"catch.wav");
 
+        Random rd = new Random();
+
+        int score = 0;
+
         public Form1()
         {
             InitializeComponent();
-            Random rd = new Random();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -20,6 +24,8 @@ namespace CatchBotton
         private void RunningBotton_MouseEnter(object sender, EventArgs e)
         {
             SystemSounds.Beep.Play();
+
+            score -= 10;
 
             // 1. 난수생성기준비
             Random rd = new Random();
@@ -33,14 +39,19 @@ namespace CatchBotton
             // 4. 위치할당(새로운Point 객체생성)
             RunningBotton.Location = new Point(nextX, nextY);
             // 5. 시각적피드백(폼제목표시줄에좌표출력)
-            this.Text = $"버튼위치: ({nextX}, {nextY})";
+            this.Text = $"버튼위치: ({nextX}, {nextY}) | 점수: {score}";
 
         }
 
         private void RunningBotton_MouseClick(object sender, MouseEventArgs e)
         {
+            score += 100;
             SystemSounds.Asterisk.Play();
             MessageBox.Show("축하합니다~!");
+
+            // 버튼 크기 10% 감소
+            RunningBotton.Width = (int)(RunningBotton.Width * 0.9);
+            RunningBotton.Height = (int)(RunningBotton.Height * 0.9);
         }
     }
 }
