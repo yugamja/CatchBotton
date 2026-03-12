47987835b9833d609a5143f417e0749348b8609b
yugamja <timun@naver.com>
2026-03-12 16:25:17 +0900

    버튼 생성 + 버튼 이동 기능 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            RunningBotton = new Button();
+            SuspendLayout();
+            // 
+            // RunningBotton
+            // 
+            RunningBotton.BackColor = Color.RosyBrown;
+            RunningBotton.Font = new Font("궁서체", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            RunningBotton.ForeColor = SystemColors.ButtonFace;
+            RunningBotton.Location = new Point(141, 147);
+            RunningBotton.Name = "RunningBotton";
+            RunningBotton.Size = new Size(233, 126);
+            RunningBotton.TabIndex = 0;
+            RunningBotton.Text = "나를 잡아봐";
+            RunningBotton.UseVisualStyleBackColor = false;
+            RunningBotton.Click += button1_Click;
+            RunningBotton.MouseClick += RunningBotton_MouseClick;
+            RunningBotton.MouseEnter += RunningBotton_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(RunningBotton);
+            Name = "Form1";
+            Text = "버튼 잡기 게임";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button RunningBotton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,32 @@ namespace CatchBotton
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RunningBotton_MouseEnter(object sender, EventArgs e)
+        {
+            // 1. 난수생성기준비
+            Random rd = new Random();
+            // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호)
+            // ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
+            int maxX = this.ClientSize.Width - RunningBotton.Width;
+            int maxY = this.ClientSize.Height - RunningBotton.Height;
+            // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+            // 4. 위치할당(새로운Point 객체생성)
+            RunningBotton.Location = new Point(nextX, nextY);
+            // 5. 시각적피드백(폼제목표시줄에좌표출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+        }
+
+        private void RunningBotton_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+        }
     }
 }
