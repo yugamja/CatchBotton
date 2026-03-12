1bedcc871605f7ee4c3c066afbfb02d1459325f4
yugamja <timun@naver.com>
2026-03-12 17:22:51 +0900

    4단계 게임오버 기능 추

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,8 @@ namespace CatchBotton
         Random rd = new Random();
 
         int score = 0;
+        // 버튼 놓친 횟수
+        int missCount = 0;
 
         public Form1()
         {
@@ -21,11 +23,40 @@ namespace CatchBotton
 
         }
 
+        private void GameOver()
+        {
+            //게임오버 함수
+            MessageBox.Show("Game Over");
+            //재시작 함수 실행
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            score = 0;
+            missCount = 0;
+
+            // 버튼 크기 초기화
+            RunningBotton.Width = 233;
+            RunningBotton.Height = 126;
+
+            // 버튼 위치 초기화
+            RunningBotton.Location = new Point(100, 100);
+
+            // 버튼 다시 활성화
+            RunningBotton.Enabled = true;
+
+            // 제목 초기화
+            this.Text = "Catch Button Game";
+        }
+
         private void RunningBotton_MouseEnter(object sender, EventArgs e)
         {
             SystemSounds.Beep.Play();
 
+            // 놓치면 점수 -10점, 놓친 횟수 +1
             score -= 10;
+            missCount++;
 
             // 1. 난수생성기준비
             Random rd = new Random();
@@ -41,7 +72,13 @@ namespace CatchBotton
             // 5. 시각적피드백(폼제목표시줄에좌표출력)
             this.Text = $"버튼위치: ({nextX}, {nextY}) | 점수: {score}";
 
+            if(missCount >= 20)
+            {
+                GameOver();
+            }
+
         }
+
 
         private void RunningBotton_MouseClick(object sender, MouseEventArgs e)
         {
